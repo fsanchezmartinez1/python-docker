364b422753bc44280752c88eb237eb218aab32be
Fernando Sánchez <fsanchezmartinez1@educa.madrid.org>
2024-02-15 18:14:07 +0100

    suma

--- src/tablaMultiplicacion.py
+++ src/tablaMultiplicacion.py
@@ -1,18 +1,21 @@
 import random
 
-numero = 7
+numeroPreguntas = random.randint(1,8)
 
-for i in range(1,11):
-    print(str(numero)+"x"+str(i)+"="+str(numero*i))
+aciertos = 0 
+fallos = 0
+for i in range(1,numeroPreguntas+1):
+    numero = random.randint(2,9)
+    multiplicando = random.randint(1,10)
+    respuesta = input("¿Cual es el resultado de multiplicar "+str(numero)+"x"+str(multiplicando)+"=")
+
+    if(int(respuesta) == numero*multiplicando):
+        aciertos = aciertos + 1
+    else:
+        fallos = fallos + 1
 
 
-numeroPreguntas = 10
+nota = (aciertos*10)/numeroPreguntas
 
-
-multiplicando = random.randint(1,10)
-respuesta = input("¿Cual es el resultado de multiplicar "+str(numero)+"x"+str(multiplicando)+"=")
-
-if(int(respuesta) == numero*multiplicando):
-    print("Muy bien")
-else:
-    print("Muy mal")
+print("El resultado obtenido es de "+str(aciertos)+" aciertos")
+print("Tu nota es de "+str(nota))
